47f3081bab74ed49aa4649e6696f76886e4730ad
isidoreduc <isidoreducasse79@gmail.com>
2019-09-22 13:50:03 +0100

    mongoose setup

--- app.js
+++ app.js
@@ -7,13 +7,12 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 // app.use() to add middleware
-app.use(morgan('dev')); // show a summary of the request - response
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(morgan('dev'));
+} // logs a summary of the request - response
 app.use(express.json());
-// app.use((req, res, next) => {
-//   console.log('hello from middleware');
-//   next();
-// });
-
+// using middleware to serve static files
+app.use(express.static(`${__dirname}/public`));
 // Routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const app = require('./app');
-const port = 3000;
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// path to environment variables config
+dotenv.config({ path: './config.env' });
+const port = process.env.PORT || 3000;
+
+// mongoose connecection
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('Connection successfull'));
 
 // server listening for requests
 app.listen(port, () => console.log(`app runing on port ${port}...`));
