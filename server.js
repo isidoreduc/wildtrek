9d35ed8b8edf49faa1c8d5da11130e31e1b56b2f
isidoreduc <isidoreducasse79@gmail.com>
2019-09-23 23:40:08 +0100

    mongoose model

--- server.js
+++ server.js
@@ -16,5 +16,22 @@ mongoose
   })
   .then(() => console.log('Connection successfull'));
 
+const tourSchema = mongoose.Schema({
+  name: { type: String, required: [true, 'Name is mandatory'], unique: true },
+  rating: { type: Number, default: 4.5 },
+  price: { type: Number, require: [true, 'Price is mandatory'] }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'Ceahlau',
+  price: 234
+});
+
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log(err));
+
 // server listening for requests
 app.listen(port, () => console.log(`app runing on port ${port}...`));
